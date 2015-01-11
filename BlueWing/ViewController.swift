c99a9e23dbcaeea0c879a35abbaecd21fbcd9b82
adisha94 <abetawar@gmail.com>
2015-01-10 21:11:32 -0800

    Initial Commit

---
+++ BlueWing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlueWing
+//
+//  Created by Adish Betawar on 1/10/15.
+//  Copyright (c) 2015 Mega Saga Entertainment. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
